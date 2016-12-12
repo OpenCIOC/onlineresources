63fface452afbf92268285fc148fe894616eecf9
Christopher Lambacher <chris@kateandchris.net>
2016-12-11 22:22:24 -0500

    Fix primary key for CIC_View_Whitelist

--- python/sql/core/Tables/dbo.CIC_View_Whitelist.sql
+++ python/sql/core/Tables/dbo.CIC_View_Whitelist.sql
@@ -3,8 +3,8 @@ CREATE TABLE [dbo].[CIC_View_Whitelist]
 [IPAddress] [varchar] (20) COLLATE Latin1_General_100_CI_AI NOT NULL,
 [ViewType] [int] NOT NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[CIC_View_Whitelist] ADD CONSTRAINT [PK__CIC_View_Banned] PRIMARY KEY CLUSTERED  ([IPAddress]) ON [PRIMARY]
+ALTER TABLE [dbo].[CIC_View_Whitelist] ADD 
+CONSTRAINT [PK_CIC_View_Whitelist] PRIMARY KEY CLUSTERED  ([IPAddress], [ViewType]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[CIC_View_Whitelist] ADD CONSTRAINT [FK_CIC_View_Whitelist_CIC_View] FOREIGN KEY ([ViewType]) REFERENCES [dbo].[CIC_View] ([ViewType])
 GO
