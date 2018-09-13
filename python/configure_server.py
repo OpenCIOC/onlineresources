55fd0fe1b77863ee0f3aa7a95b23bef8a6a9217d
Christopher Lambacher <chris@kateandchris.net>
2018-09-13 18:03:33 -0400

    Fix config file filling

--- python/configure_server.py
+++ python/configure_server.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import argparse
 
-site_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+site_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 config_tmpl = '''
 [global]
