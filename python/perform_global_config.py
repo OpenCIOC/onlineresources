a2e24c62d37e5b56964334d81ffa133e5b0ab7f9
Christopher Lambacher <chris@kateandchris.net>
2018-09-13 00:12:38 -0400

    Fix istall of features

--- python/perform_global_config.py
+++ python/perform_global_config.py
@@ -43,7 +43,6 @@ def main():
 	#subprocess.call([sys.executable, '-m', 'pip', 'install', 'virtualenv'])
 	#subprocess.call([sys.executable, '-m', 'pip', 'install', 'virtualenvwrapper-win'])
 	redis_dir = os.path.join('c:\\program files\\', 'redis')
-	print "redis_dir", redis_dir
 
 	subprocess.call(
 		[
@@ -53,14 +52,14 @@ def main():
 		cwd=redis_dir
 	)
 
-	features = 'IIS-WebServerRole IIS-WebServer IIS-ISAPIExtensions IIS-ASP IIS-ASPNET45 IIS-HttpCompressionDynamic IIS-Performance Smtpsvc-Service-Update-Name Smtpsvc-Admin-Update-Name IIS-IPSecurity IIS-CGI IIS-ManagementScriptingTools IIS-LegacyScripts IIS-HttpTracing IIS-RequestMonitor'.split()
+	features = 'IIS-WebServerRole IIS-WebServer IIS-ISAPIExtensions IIS-ASP IIS-ASPNET45 IIS-HttpCompressionDynamic IIS-Performance IIS-IPSecurity IIS-CGI IIS-ManagementScriptingTools IIS-LegacyScripts IIS-HttpTracing IIS-RequestMonitor'.split()
 	features = ['/FeatureName:' + x for x in features]
 	subprocess.call(['dism', '/Online', '/Enable-Feature'] + features + ['/All'])
 
 	webpi_exe = os.path.join(os.environ['ProgramW6432'], 'Microsoft/Web Platform Installer/WebpiCmd.exe')
 
 	subprocess.call([webpi_exe, '/Install', '/products:ARRv3_0', '/accepteula'])
-	appcmd_exe = os.path.join(os.environ['systemroot'], 'system32/inetsrv/appcmd.exe')
+	appcmd_exe = 'c:\\windows\\system32\\inetsrv\\appcmd.exe'
 	subprocess.call([
 		appcmd_exe, 'set', 'config', '-section:system.webServer/proxy',
 		'/enabled:True', '/includePortInXForwardedFor:False',
