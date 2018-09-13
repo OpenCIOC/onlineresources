8289b7b1cc6d4e057c20eef9665232cc3941ec74
Christopher Lambacher <chris@kateandchris.net>
2018-09-13 16:58:28 -0400

    Fix configure_site

--- python/configure_site.py
+++ python/configure_site.py
@@ -3,7 +3,7 @@ import argparse
 import shutil
 import subprocess
 
-site_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+site_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 
 def parse_args():
