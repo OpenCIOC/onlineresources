db8d954d70e5f7420a7719e778dc67f13e73919b
Christopher Lambacher <chris@kateandchris.net>
2021-01-31 12:00:47 -0500

    Upgrade PDF infrastructure and stop gap on static maps

--- python/cioc/core/renderpdf.py
+++ python/cioc/core/renderpdf.py
@@ -15,12 +15,15 @@
 # =========================================================================================
 
 from __future__ import absolute_import
+import six
 import pdfkit
 
 
 def render_to_pdf(request, html, footer_file):
 	wkhtmltopdfpath = request.config.get('wkhtmltopdfpath', r'c:\Program Files (x86)\wkhtmltopdf\bin\wkhtmltopdf.exe')
 	config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdfpath)
+	if isinstance(html, six.binary_type):
+		html = html.decode('utf-8')
 	return pdfkit.from_string(html, False, configuration=config, options={
 		'footer-html': footer_file, 'margin-bottom': request.viewdata.dom.PDFBottomMargin,
 		'page-size': 'letter', 'disable-javascript': None, 'print-media-type': None,
