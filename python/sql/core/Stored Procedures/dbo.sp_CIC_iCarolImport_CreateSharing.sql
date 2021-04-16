f661e580dcc6d26fe4f7ffdb6e5b13da643b87e5
Christopher Lambacher <chris@kateandchris.net>
2021-04-16 14:42:32 -0400

    Handle iCarol IsPrivate

--- python/sql/core/Stored Procedures/dbo.sp_CIC_iCarolImport_CreateSharing.sql
+++ python/sql/core/Stored Procedures/dbo.sp_CIC_iCarolImport_CreateSharing.sql
@@ -37,8 +37,8 @@ SELECT CAST((SELECT (
 		(SELECT a.DisabilitiesAccess AS [@N], f.DisabilitiesAccess AS [@NF] FOR XML PATH('ACCESSIBILITY'), TYPE),
 		(SELECT a.LicenseAccreditation AS [@V], f.LicenseAccreditation AS [@VF] FOR XML PATH('ACCREDITED'), TYPE),
 		(SELECT
-			 CASE WHEN a.PhoneNumberAfterHours IS NOT NULL AND a.PhoneNumberAfterHoursDescription  IS NOT NULL THEN a.PhoneNumberAfterHoursDescription + ': ' ELSE '' END + a.PhoneNumberAfterHours AS [@V],
-			 CASE WHEN a.PhoneNumberAfterHours IS NOT NULL AND f.PhoneNumberAfterHoursDescription  IS NOT NULL THEN f.PhoneNumberAfterHoursDescription + ': ' ELSE '' END + f.PhoneNumberAfterHours AS [@VF]
+			 CASE WHEN a.PhoneNumberAfterHoursIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneNumberAfterHours IS NOT NULL AND a.PhoneNumberAfterHoursDescription  IS NOT NULL THEN a.PhoneNumberAfterHoursDescription + ': ' ELSE '' END + a.PhoneNumberAfterHours END AS [@V],
+			 CASE WHEN a.PhoneNumberAfterHoursIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneNumberAfterHours IS NOT NULL AND f.PhoneNumberAfterHoursDescription  IS NOT NULL THEN f.PhoneNumberAfterHoursDescription + ': ' ELSE '' END + f.PhoneNumberAfterHours END AS [@VF]
 		  FOR XML PATH('AFTER_HRS_PHONE'), TYPE), 
 		(SELECT 
 				(SELECT
@@ -79,18 +79,18 @@ SELECT CAST((SELECT (
 		(SELECT a.BusServiceAccess AS [@N], f.BusServiceAccess AS [@NF] FOR XML PATH('BUS_ROUTES'),TYPE),
 		(SELECT a.InternalNotesForEditorsAndViewers AS [@V], f.InternalNotesForEditorsAndViewers AS [@VF] FOR XML PATH('COMMENTS'), TYPE),
 		(SELECT 
-			(SELECT 'E' AS [@LANG], a.MainContactName AS [@NMLAST], a.MainContactTitle AS [@TTL], a.MainContactPhoneNumber AS [@PH1N], a.MainContactEmailAddress AS [@EML]
+			(SELECT 'E' AS [@LANG], CASE WHEN a.MainContactIsPrivate = 'Yes' THEN NULL ELSE a.MainContactName END AS [@NMLAST], a.MainContactTitle AS [@TTL], a.MainContactPhoneNumber AS [@PH1N], a.MainContactEmailAddress AS [@EML]
 			-- NOTE a.MainContactType didn't have any data in it so it was not mapped
-			WHERE a.MainContactName IS NOT NULL OR a.MainContactTitle IS NOT NULL OR a.MainContactPhoneNumber IS NOT NULL OR a.MainContactEmailAddress IS NOT NULL
+			WHERE COALESCE(a.MainContactIsPrivate, 'No') = 'No' AND (a.MainContactName IS NOT NULL OR a.MainContactTitle IS NOT NULL OR a.MainContactPhoneNumber IS NOT NULL OR a.MainContactEmailAddress IS NOT NULL)
 			FOR XML PATH('CONTACT'), TYPE),
 			(SELECT 'F' AS [@LANG], f.MainContactName AS [@NMLAST], f.MainContactTitle AS [@TTL], f.MainContactPhoneNumber AS [@PH1N], f.MainContactEmailAddress AS [@EML]
 			-- NOTE a.MainContactType didn't have any data in it so it was not mapped
-			WHERE f.MainContactName IS NOT NULL OR f.MainContactTitle IS NOT NULL OR f.MainContactPhoneNumber IS NOT NULL OR f.MainContactEmailAddress IS NOT NULL
+			WHERE COALESCE(f.MainContactIsPrivate, 'No') = 'No' AND (f.MainContactName IS NOT NULL OR f.MainContactTitle IS NOT NULL OR f.MainContactPhoneNumber IS NOT NULL OR f.MainContactEmailAddress IS NOT NULL)
 			FOR XML PATH('CONTACT'), TYPE)
 		FOR XML PATH('CONTACT_1') ,TYPE),
 		(SELECT
-			 CASE WHEN a.PhoneNumberHotline IS NOT NULL AND a.PhoneNumberHotlineDescription  IS NOT NULL THEN a.PhoneNumberHotlineDescription + ': ' ELSE '' END + a.PhoneNumberHotline AS [@V],
-			 CASE WHEN f.PhoneNumberHotline IS NOT NULL AND f.PhoneNumberHotlineDescription  IS NOT NULL THEN f.PhoneNumberHotlineDescription + ': ' ELSE '' END + f.PhoneNumberHotline AS [@VF]
+			 CASE WHEN a.PhoneNumberHotlineIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneNumberHotline IS NOT NULL AND a.PhoneNumberHotlineDescription  IS NOT NULL THEN a.PhoneNumberHotlineDescription + ': ' ELSE '' END + a.PhoneNumberHotline END AS [@V],
+			 CASE WHEN f.PhoneNumberHotlineIsPrivate = 'True' THEN NULL ELSE CASE WHEN f.PhoneNumberHotline IS NOT NULL AND f.PhoneNumberHotlineDescription  IS NOT NULL THEN f.PhoneNumberHotlineDescription + ': ' ELSE '' END + f.PhoneNumberHotline END AS [@VF]
 		  FOR XML PATH('CRISIS_PHONE'), TYPE), 
 		(SELECT a.DELETION_DATE AS [@V], f.DELETION_DATE AS [@VF] FOR XML PATH('DELETION_DATE'), TYPE),
 		(SELECT a.DESCRIPTION AS [@V], f.DESCRIPTION AS [@VF] FOR XML PATH('DESCRIPTION'), TYPE),
@@ -100,22 +100,22 @@ SELECT CAST((SELECT (
 		(SELECT a.YearIncorporated AS [@V], f.YearIncorporated AS [@VF] FOR XML PATH('ESTABLISHED'), TYPE),
 		(SELECT
 			(SELECT 'E' AS [@LANG], a.SeniorWorkerName AS [@NMLAST], a.SeniorWorkerTitle AS [@TTL], a.SeniorWorkerPhoneNumber AS [@PH1N], a.SeniorWorkerEmailAddress AS [@EML]
-				WHERE a.SeniorWorkerName IS NOT NULL OR a.SeniorWorkerTitle IS NOT NULL OR a.SeniorWorkerPhoneNumber IS NOT NULL OR a.SeniorWorkerEmailAddress IS NOT NULL
+				WHERE COALESCE(a.SeniorWorkerIsPrivate, 'No') = 'No' AND (a.SeniorWorkerName IS NOT NULL OR a.SeniorWorkerTitle IS NOT NULL OR a.SeniorWorkerPhoneNumber IS NOT NULL OR a.SeniorWorkerEmailAddress IS NOT NULL)
 			 FOR XML PATH('CONTACT'), TYPE),
 			(SELECT 'F' AS [@LANG], f.SeniorWorkerName AS [@NMLAST], f.SeniorWorkerTitle AS [@TTL], f.SeniorWorkerPhoneNumber AS [@PH1N], f.SeniorWorkerEmailAddress AS [@EML]
-				WHERE f.SeniorWorkerName IS NOT NULL OR f.SeniorWorkerTitle IS NOT NULL OR f.SeniorWorkerPhoneNumber IS NOT NULL OR f.SeniorWorkerEmailAddress IS NOT NULL
+				WHERE  COALESCE(f.SeniorWorkerIsPrivate, 'No') = 'No' AND (f.SeniorWorkerName IS NOT NULL OR f.SeniorWorkerTitle IS NOT NULL OR f.SeniorWorkerPhoneNumber IS NOT NULL OR f.SeniorWorkerEmailAddress IS NOT NULL)
 			 FOR XML PATH('CONTACT'), TYPE)
 		 FOR XML PATH('EXEC_1'),TYPE),
 		 -- MadeInactiveOn comes in as an iso formated datetime but with a space instead of a T in between the date and time parts
 		(SELECT 'ICAROLMADEINACTIVEON' AS [@FLD], CASE WHEN a.AgencyStatus = 'Inactive' THEN REPLACE(a.MadeInactiveOn, ' ', 'T') ELSE NULL END AS [@V] FOR XML PATH('EXTRA_DATE'), TYPE),
 		(SELECT 'ICAROLAGENCYSTATUS' AS [@FLD], CASE WHEN a.AgencyStatus = 'Active' THEN 'ACTIVE' WHEN a.AgencyStatus = 'Active, but do not refer' THEN 'DO_NOT_REFER' WHEN a.AgencyStatus = 'Inactive' THEN 'INACTIVE' ELSE a.AgencyStatus END AS [@CD] FOR XML PATH('EXTRA_DROPDOWN'), TYPE),  
-		(SELECT a.PhoneFax AS [@V], f.PhoneFax AS [@VF] FOR XML PATH('FAX'), TYPE),
+		(SELECT CASE WHEN a.PhoneFaxIsPrivate = 'True' THEN NULL ELSE a.PhoneFax END AS [@V], CASE WHEN f.PhoneFaxIsPrivate = 'True' THEN NULL ELSE f.PhoneFax END AS [@VF] FOR XML PATH('FAX'), TYPE),
 		(SELECT a.FeeStructureSource AS [@N], f.FeeStructureSource AS [@NF] FOR XML PATH('FEES'), TYPE),
 		(SELECT
 		-- I think this is now handled in the rollup so we can go back to checking for NULL
-			 CASE WHEN a.Latitude IS NOT NULL AND a.Longitude IS NOT NULL THEN 3 ELSE 0 END AS [@TYPE],
-			 CAST(a.Latitude AS NUMERIC(11,7))  AS [@LAT],
-			 CAST(a.Longitude AS NUMERIC(11,7)) AS [@LONG]
+			 CASE WHEN a.Latitude IS NOT NULL AND a.Longitude IS NOT NULL AND COALESCE(a.PhysicalAddressIsPrivate, 'No') = 'No' THEN 3 ELSE 0 END AS [@TYPE],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE CAST(a.Latitude AS NUMERIC(11,7)) END AS [@LAT],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE CAST(a.Longitude AS NUMERIC(11,7)) END AS [@LONG]
 		  FOR XML PATH('GEOCODE'), TYPE),
 		(SELECT COALESCE(a.HoursOfOperation, a.Hours) AS [@V], COALESCE(f.HoursOfOperation, f.Hours) AS [@VF] FOR XML PATH('HOURS'), TYPE),
 		(SELECT
@@ -142,20 +142,20 @@ SELECT CAST((SELECT (
 		(SELECT a.LOCATION_NAME AS [@V], f.LOCATION_NAME AS [@VF] FOR XML PATH('LOCATION_NAME'), TYPE),
 		(SELECT DISTINCT irr.ResourceAgencyNum AS [@V] FROM dbo.CIC_iCarolImportRollup irr WHERE a.TaxonomyLevelName = 'Site' AND irr.ConnectsToSiteNum=a.ResourceAgencyNum AND irr.TaxonomyLevelName='ProgramAtSite' AND irr.DELETION_DATE IS NULL FOR XML PATH('SERVICE_NUM'), ROOT('LOCATION_SERVICES'), TYPE),
 		(SELECT 
-			 a.MailingAddress1 AS [@LN1],
-			 a.MailingAddress2 AS [@LN2],
-			 a.MailingCity AS [@CTY],
-			 a.MailingStateProvince AS [@PRV],
-			 CASE WHEN a.MailingPostalCode LIKE '[a-zA-Z][0-9][a-zA-Z][0-9][a-zA-Z][0-9]' THEN UPPER(LEFT(a.MailingPostalCode,3) + ' ' + RIGHT(a.MailingPostalCode, 3)) ELSE UPPER(a.MailingPostalCode) END AS [@PC],
-			 CASE WHEN COALESCE(a.MailingAddress1, a.MailingAddress2, a.MailingCity, a.MailingStateProvince, a.MailingPostalCode) IS NOT NULL THEN a.MailingCountry ELSE NULL END AS [@CTRY],
+			 CASE WHEN a.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE a.MailingAddress1 END AS [@LN1],
+			 CASE WHEN a.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE a.MailingAddress2 END AS [@LN2],
+			 CASE WHEN a.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE a.MailingCity END AS [@CTY],
+			 CASE WHEN a.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE a.MailingStateProvince END AS [@PRV],
+			 CASE WHEN a.MailingAddressIsPrivate = 'Yes' OR f.MailingAddressIsPrivate = 'Yes' THEN NULL WHEN a.MailingPostalCode LIKE '[a-zA-Z][0-9][a-zA-Z][0-9][a-zA-Z][0-9]' THEN UPPER(LEFT(a.MailingPostalCode,3) + ' ' + RIGHT(a.MailingPostalCode, 3)) ELSE UPPER(a.MailingPostalCode) END AS [@PC],
+			 CASE WHEN COALESCE(a.MailingAddressIsPrivate, 'No') = 'No' AND COALESCE(a.MailingAddress1, a.MailingAddress2, a.MailingCity, a.MailingStateProvince, a.MailingPostalCode) IS NOT NULL THEN a.MailingCountry ELSE NULL END AS [@CTRY],
 
-			 f.MailingAddress1 AS [@LN1F],
-			 f.MailingAddress2 AS [@LN2F],
-			 f.MailingCity AS [@CTYF],
-			 f.MailingStateProvince AS [@PRVF],
+			 CASE WHEN f.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE f.MailingAddress1 END AS [@LN1F],
+			 CASE WHEN f.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE f.MailingAddress2 END AS [@LN2F],
+			 CASE WHEN f.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE f.MailingCity END AS [@CTYF],
+			 CASE WHEN f.MailingAddressIsPrivate = 'Yes' THEN NULL ELSE f.MailingStateProvince END AS [@PRVF],
 			 -- NOTE: Postal Code is shared
 			 -- f.MailingPostalCode AS [@PC],
-			 CASE WHEN COALESCE(f.MailingAddress1, f.MailingAddress2, f.MailingCity, f.MailingStateProvince, a.MailingPostalCode) IS NOT NULL THEN f.MailingCountry ELSE NULL END AS [@CTRYF]
+			 CASE WHEN COALESCE(f.MailingAddressIsPrivate, 'No') = 'No' AND COALESCE(f.MailingAddress1, f.MailingAddress2, f.MailingCity, f.MailingStateProvince, a.MailingPostalCode) IS NOT NULL THEN f.MailingCountry ELSE NULL END AS [@CTRYF]
 			FOR XML PATH('MAIL_ADDRESS'), TYPE
 		),
 		(SELECT 
@@ -165,36 +165,36 @@ SELECT CAST((SELECT (
 		(SELECT
 			 (SELECT STUFF((SELECT '; ' + NumberValue FROM (
 
-			SELECT CASE WHEN a.PhoneNumberBusinessLine  IS NOT NULL AND a.PhoneNumberBusinessLineDescription  IS NOT NULL THEN a.PhoneNumberBusinessLineDescription + ': ' ELSE '' END + a.PhoneNumberBusinessLine AS NumberValue
+			SELECT CASE WHEN a.PhoneNumberBusinessLineIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneNumberBusinessLine  IS NOT NULL AND a.PhoneNumberBusinessLineDescription  IS NOT NULL THEN a.PhoneNumberBusinessLineDescription + ': ' ELSE '' END + a.PhoneNumberBusinessLine END AS NumberValue
 			UNION ALL SELECT 
-				CASE WHEN PhoneNumber IS NOT NULL AND PhoneName IS NOT NULL THEN PhoneName + ': ' ELSE '' END + PhoneNumber AS NumberValue
+				CASE WHEN PhoneIsPrivate = 'True' THEN NULL ELSE CASE WHEN PhoneNumber IS NOT NULL AND PhoneName IS NOT NULL THEN PhoneName + ': ' ELSE '' END + PhoneNumber END AS NumberValue
 				FROM (
 					VALUES 
-						(a.Phone1Number, a.Phone1Name),
-						(a.Phone2Number, a.Phone2Name),
-						(a.Phone3Number, a.Phone3Name),
-						(a.Phone4Number, a.Phone4Name),
-						(a.Phone5Number, a.Phone5Name),
-						(a.PhoneNumberOutOfArea, a.PhoneNumberOutOfAreaDescription)
-					) AS cte (PhoneNumber, PhoneName)
+						(a.Phone1Number, a.Phone1Name, COALESCE(a.Phone1IsPrivate, 'False')),
+						(a.Phone2Number, a.Phone2Name, COALESCE(a.Phone2IsPrivate, 'False')),
+						(a.Phone3Number, a.Phone3Name, COALESCE(a.Phone3IsPrivate, 'False')),
+						(a.Phone4Number, a.Phone4Name, COALESCE(a.Phone4IsPrivate, 'False')),
+						(a.Phone5Number, a.Phone5Name, COALESCE(a.Phone5IsPrivate, 'False')),
+						(a.PhoneNumberOutOfArea, a.PhoneNumberOutOfAreaDescription, COALESCE(a.PhoneNumberOutOfAreaIsPrivate, 'False'))
+					) AS cte (PhoneNumber, PhoneName, PhoneIsPrivate)
 					WHERE cte.PhoneNumber IS NOT NULL
 				) AS i FOR XML PATH(''),TYPE).value('.', 'nvarchar(max)'), 1, 2, '')
 			
 			 ) AS [@V],
 			 (SELECT STUFF((SELECT '; ' + NumberValue FROM (
 
-			SELECT CASE WHEN f.PhoneNumberBusinessLine  IS NOT NULL AND f.PhoneNumberBusinessLineDescription  IS NOT NULL THEN f.PhoneNumberBusinessLineDescription + ': ' ELSE '' END + f.PhoneNumberBusinessLine AS NumberValue
+			SELECT CASE WHEN f.PhoneNumberBusinessLineIsPrivate = 'True' THEN NULL ELSE CASE WHEN f.PhoneNumberBusinessLine  IS NOT NULL AND f.PhoneNumberBusinessLineDescription  IS NOT NULL THEN f.PhoneNumberBusinessLineDescription + ': ' ELSE '' END + f.PhoneNumberBusinessLine END AS NumberValue
 			UNION ALL SELECT 
-				CASE WHEN PhoneNumber IS NOT NULL AND PhoneName IS NOT NULL THEN PhoneName + ': ' ELSE '' END + PhoneNumber AS NumberValue
+				CASE WHEN PhoneIsPrivate = 'True' THEN NULL ELSE CASE WHEN PhoneNumber IS NOT NULL AND PhoneName IS NOT NULL THEN PhoneName + ': ' ELSE '' END + PhoneNumber END AS NumberValue
 				FROM (
 					VALUES 
-						(f.Phone1Number, f.Phone1Name),
-						(f.Phone2Number, f.Phone2Name),
-						(f.Phone3Number, f.Phone3Name),
-						(f.Phone4Number, f.Phone4Name),
-						(f.Phone5Number, f.Phone5Name),
-						(f.PhoneNumberOutOfArea, f.PhoneNumberOutOfAreaDescription)
-					) AS cte (PhoneNumber, PhoneName)
+						(f.Phone1Number, f.Phone1Name, COALESCE(f.Phone1IsPrivate, 'False')),
+						(f.Phone2Number, f.Phone2Name, COALESCE(f.Phone2IsPrivate, 'False')),
+						(f.Phone3Number, f.Phone3Name, COALESCE(f.Phone3IsPrivate, 'False')),
+						(f.Phone4Number, f.Phone4Name, COALESCE(f.Phone4IsPrivate, 'False')),
+						(f.Phone5Number, f.Phone5Name, COALESCE(f.Phone5IsPrivate, 'False')),
+						(f.PhoneNumberOutOfArea, f.PhoneNumberOutOfAreaDescription, COALESCE(f.PhoneNumberOutOfAreaIsPrivate, 'False'))
+					) AS cte (PhoneNumber, PhoneName, PhoneIsPrivate)
 					WHERE cte.PhoneNumber IS NOT NULL
 				) AS i FOR XML PATH(''),TYPE).value('.', 'nvarchar(max)') , 1, 2, '')
 			
@@ -208,21 +208,21 @@ SELECT CAST((SELECT (
 		(SELECT a.SERVICE_NAME_LEVEL_1 AS [@V], f.SERVICE_NAME_LEVEL_1 AS [@VF] FOR XML PATH('SERVICE_NAME_LEVEL_1'), TYPE),
 		(SELECT CASE WHEN a.SERVICE_NAME_LEVEL_2 <> a.SERVICE_NAME_LEVEL_1 THEN a.SERVICE_NAME_LEVEL_2 ELSE NULL END AS [@V], CASE WHEN f.SERVICE_NAME_LEVEL_2 <> f.SERVICE_NAME_LEVEL_1 THEN f.SERVICE_NAME_LEVEL_2 ELSE NULL END AS [@VF] FOR XML PATH('SERVICE_NAME_LEVEL_2'), TYPE),
 		(SELECT 
-			 a.PhysicalAddress1 AS [@LN1],
-			 a.PhysicalAddress2 AS [@LN2],
-			 a.PhysicalCity AS [@CTY],
-			 a.PhysicalStateProvince AS [@PRV],
-			 CASE WHEN a.PhysicalPostalCode LIKE '[a-zA-Z][0-9][a-zA-Z][0-9][a-zA-Z][0-9]' THEN UPPER(LEFT(a.PhysicalPostalCode,3) + ' ' + RIGHT(a.PhysicalPostalCode, 3)) ELSE UPPER(a.PhysicalPostalCode) END AS [@PC],
-			 CASE WHEN COALESCE(a.PhysicalAddress1, a.PhysicalAddress2, a.PhysicalCity, a.PhysicalStateProvince, a.PhysicalPostalCode ) IS NOT NULL THEN a.PhysicalCountry ELSE NULL END AS [@CTRY],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE a.PhysicalAddress1 END AS [@LN1],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE a.PhysicalAddress2 END AS [@LN2],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE a.PhysicalCity END AS [@CTY],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE a.PhysicalStateProvince END AS [@PRV],
+			 CASE WHEN a.PhysicalAddressIsPrivate = 'Yes' OR f.PhysicalAddressIsPrivate = 'Yes' THEN NULL WHEN a.PhysicalPostalCode LIKE '[a-zA-Z][0-9][a-zA-Z][0-9][a-zA-Z][0-9]' THEN UPPER(LEFT(a.PhysicalPostalCode,3) + ' ' + RIGHT(a.PhysicalPostalCode, 3)) ELSE UPPER(a.PhysicalPostalCode) END AS [@PC],
+			 CASE WHEN COALESCE(a.PhysicalAddressIsPrivate, 'No') = 'No' AND COALESCE(a.PhysicalAddress1, a.PhysicalAddress2, a.PhysicalCity, a.PhysicalStateProvince, a.PhysicalPostalCode ) IS NOT NULL THEN a.PhysicalCountry ELSE NULL END AS [@CTRY],
 
-			 f.PhysicalAddress1 AS [@LN1F],
-			 f.PhysicalAddress2 AS [@LN2F],
-			 f.PhysicalCity AS [@CTYF],
-			 f.PhysicalStateProvince AS [@PRVF],
+			 CASE WHEN f.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE f.PhysicalAddress1 END AS [@LN1F],
+			 CASE WHEN f.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE f.PhysicalAddress2 END AS [@LN2F],
+			 CASE WHEN f.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE f.PhysicalCity END AS [@CTYF],
+			 CASE WHEN f.PhysicalAddressIsPrivate = 'Yes' THEN NULL ELSE f.PhysicalStateProvince END AS [@PRVF],
 
 			 -- Only one Postal Code Value Allowed, using English
 			 -- f.PhysicalPostalCode AS [@PC],
-			 CASE WHEN COALESCE(f.PhysicalAddress1, f.PhysicalAddress2, f.PhysicalCity, f.PhysicalStateProvince, f.PhysicalPostalCode ) IS NOT NULL THEN f.PhysicalCountry ELSE NULL END AS [@CTRYF]
+			 CASE WHEN COALESCE(f.PhysicalAddressIsPrivate, 'No') = 'No' AND COALESCE(f.PhysicalAddress1, f.PhysicalAddress2, f.PhysicalCity, f.PhysicalStateProvince, f.PhysicalPostalCode ) IS NOT NULL THEN f.PhysicalCountry ELSE NULL END AS [@CTRYF]
 			FOR XML PATH('SITE_ADDRESS'), TYPE
 		),
 		(SELECT
@@ -317,12 +317,12 @@ SELECT CAST((SELECT (
 			FOR XML PATH('LNK'), TYPE)
 		FOR XML PATH('TAXONOMY'), TYPE),
 		(SELECT
-			 CASE WHEN a.PhoneTTY  IS NOT NULL AND a.PhoneTTYDescription  IS NOT NULL THEN a.PhoneTTYDescription + ': ' ELSE '' END + a.PhoneTTY AS [@V],
-			 CASE WHEN f.PhoneTTY  IS NOT NULL AND f.PhoneTTYDescription  IS NOT NULL THEN f.PhoneTTYDescription + ': ' ELSE '' END + f.PhoneTTY AS [@VF]
+			 CASE WHEN a.PhoneTTYIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneTTY  IS NOT NULL AND a.PhoneTTYDescription  IS NOT NULL THEN a.PhoneTTYDescription + ': ' ELSE '' END + a.PhoneTTY END AS [@V],
+			 CASE WHEN f.PhoneTTYIsPrivate = 'True' THEN NULL ELSE CASE WHEN f.PhoneTTY  IS NOT NULL AND f.PhoneTTYDescription  IS NOT NULL THEN f.PhoneTTYDescription + ': ' ELSE '' END + f.PhoneTTY END AS [@VF]
 		  FOR XML PATH('TDD_PHONE'), TYPE),
 		(SELECT 
-			CASE WHEN a.PhoneTollFree  IS NOT NULL AND a.PhoneTollFreeDescription  IS NOT NULL THEN a.PhoneTollFreeDescription + ': ' ELSE '' END + a.PhoneTollFree AS [@V],
-			CASE WHEN f.PhoneTollFree  IS NOT NULL AND f.PhoneTollFreeDescription  IS NOT NULL THEN f.PhoneTollFreeDescription + ': ' ELSE '' END + f.PhoneTollFree AS [@VF]
+			CASE WHEN a.PhoneTollFreeIsPrivate = 'True' THEN NULL ELSE CASE WHEN a.PhoneTollFree  IS NOT NULL AND a.PhoneTollFreeDescription  IS NOT NULL THEN a.PhoneTollFreeDescription + ': ' ELSE '' END + a.PhoneTollFree END AS [@V],
+			CASE WHEN f.PhoneTollFreeIsPrivate = 'True' THEN NULL ELSE CASE WHEN f.PhoneTollFree  IS NOT NULL AND f.PhoneTollFreeDescription  IS NOT NULL THEN f.PhoneTollFreeDescription + ': ' ELSE '' END + f.PhoneTollFree END AS [@VF]
 		 FOR XML PATH('TOLL_FREE_PHONE'), TYPE),
 		(SELECT REPLACE(a.LastVerifiedOn, ' ', 'T') AS [@V], REPLACE(a.LastVerifiedOn, ' ', 'T') AS [@VF] FOR XML PATH('UPDATE_DATE'), TYPE),
 		(SELECT NULLIF(a.LastVerificationApprovedBy, 'Unspecified Unspecified') AS [@V], NULLIF(a.LastVerificationApprovedBy, 'Unspecified Unspecified') AS [@VF] FOR XML PATH('UPDATED_BY'), TYPE),
